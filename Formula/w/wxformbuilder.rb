cf9ecdfda2ed6e7206e037af5ad276596a4a6f56
Stefano Weidmann <stefano.weidmann@mail.ch>
2024-04-26 22:02:03 +0200

    fix

--- Formula/w/wxformbuilder.rb
+++ Formula/w/wxformbuilder.rb
@@ -11,7 +11,7 @@ class Wxformbuilder < Formula
   depends_on "wxwidgets"
 
   def install
-    inreplace "CMakeLists.txt", /set\(wxWidgetsComponentsRequired.*\)/, "set(wxWidgetsComponentsRequired all)"
+    inreplace "CMakeLists.txt", /^set\(wxWidgetsComponentsRequired.*\)/, "set(wxWidgetsComponentsRequired all)"
     system "cmake", "-S", ".", "-B", "build", *std_cmake_args
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
